c3653f130781d83363f5280055918bef5858dded
diegodf8 <diegodf8@gmail.com>
2018-03-08 08:48:53 +0000

    Subida Couu

--- src/main/java/com/cice/ficheros/App.java
+++ src/main/java/com/cice/ficheros/App.java
@@ -1,8 +1,11 @@
 package com.cice.ficheros;
 
-
 import java.io.BufferedWriter;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class App
 {
@@ -13,11 +16,30 @@ public class App
             BufferedWriter bw = new BufferedWriter(printWriter);
             bw.write("Esto es una cadena de prueba de escritura");
             bw.write("\n Salto de linasdasdasdesssssssa");
+            printWriter.println("Tercera caden3333333a");
             bw.close();
             printWriter.close();
+            System.out.println("TExt2");
+            //leerFichero("texto.txt");
         }catch (Exception ex){
             ex.printStackTrace();
         }
 
+    }
+    
+    public static void leerFichero(String RutaFichero){
+       try {
+            FileReader fileReader = new FileReader(RutaFichero);
+            BufferedReader bufferReader = new BufferedReader(fileReader);
+            String cadena = bufferReader.readLine();
+            System.out.println(cadena);
+            System.out.println("TExt");
+            bufferReader.close();
+            fileReader.close();
+        }catch (FileNotFoundException ex){
+            ex.printStackTrace();
+        }catch (IOException ex2){
+            ex2.printStackTrace();
+        }
     }
 }
